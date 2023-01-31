882435a1921c74f12b55adf9414303a641e1063b
hubert <haik.rpk10@gmail.com>
2023-01-31 13:04:16 +0100

    initial commit

---
+++ strona.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import mysql.connector
+
+connection = mysql.connector.connect(host='127.0.0.1', user='root',
+                                     password='', database='sys')
+
+cursor = connection.cursor()
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hodowle")
+def hodowle():
+    cursor.execute("select * from hodowle")
+    value = cursor.fetchall()
+    return render_template("hodowle.html", data=value)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
